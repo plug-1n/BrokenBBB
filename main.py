ac0293bd8a173cadff5dc8866e1c847ff5b5d269
Ed Khugaev <ed.khugaev@gmail.com>
2023-06-19 15:14:16 +0300

    clear structure of dividing phrases

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 from gtts import gTTS
 from urllib import request as ur
 from  moviepy import editor as mp
+from pydub import AudioSegment
  
 config_data = None
 with open("config.yaml", "r") as stream:
@@ -18,29 +19,47 @@ host = config_data['url_host']
 headers ={"X-Csrf-Token":csrf_token}
 input_text = input('Input text: ')
 split_text = input_text.split()
-
+size = len(split_text)
+frames = []
 if not input_text:
     logging.error("Input text is necessary")
     exit()
 
 
 
-
-
-
-def text_to_voice(arr):
-    for i in range(0,len(input_text.split())):
-        flag = False
-        for j in range(len(input_text.split()),i,-1):
-            url_format = '+'.join(split_text[i:j])
-            req = requests.get(host+f"q={url_format}&pos=1",headers=headers)
-            valid_data = req.json()
-            if valid_data['count']:
-                print(split_text[i:j])
-                flag = True
-        if not flag:    
-            myobj = gTTS(text=split_text[i:j], lang="en", slow=False)
-            myobj.save(f"welcome{str(i)}.mp3")    
+start = 0
+end = size
+frame = 1
+while start<end:
+    flag = False
+    for i in range(end,start,-1):
+        url_req = host + f"q={'+'.join(split_text[start:i])}&pos=1"
+        print(split_text[start:i],start,i)
+        req = requests.get(url_req,headers=headers)
+        valid_data = req.json()
+        if valid_data['count']:
+            print("YES")
+            flag = True
+            start = i - 1
+            break
+    if not flag:
+          myobj = AudioSegment.from_file(gTTS(text=split_text[start], lang="en", slow=False))
+          frames.append(myobj) 
+    start+=1
+print(frames)
+# for i in range(0,len(input_text.split())):
+#     flag = False
+#     for j in range(len(input_text.split()),i,-1):
+#         url_format = '+'.join(split_text[i:j])
+#         req = requests.get(host+f"q={url_format}&pos=1",headers=headers)
+#         valid_data = req.json()
+#         if valid_data['count']:
+#             print(split_text[i:j])
+#             flag = True
+#             break
+#     if not flag:    
+#         myobj = gTTS(text=split_text[i:j], lang="en", slow=False)
+#         myobj.save("welcome.mp3")    
 
 # print(valid_data)
 # if valid_data['count']:
