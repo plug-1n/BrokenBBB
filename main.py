ce11a29ea311d76bd591d3efa31701c6fd4dddb3
Ed Khugaev <ed.khugaev@gmail.com>
2023-07-05 15:50:34 +0300

    mp4 without np3

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import random
 import requests
 import urllib
+from io import BytesIO
 from views import get_config_yaml
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 
@@ -49,7 +50,28 @@ def main():
     size = len(input_text)
 
     frames = []
+
     start = 0
+    end = size
+    while start<end:
+        flag = False
+        for i in range(end, start,-1):
+            print(input_text[start:i])
+            video_bytes = play_phrase_api(input_text[start:i])
+            if video_bytes:
+                save_video_from_bytes(video_bytes,"hello.mp4")
+                frames.append(video_bytes)
+                print("SUI")
+                flag = True
+                start = i - 1
+                break
+        if not flag:
+            print("CMEREA VOVO")
+        start+=1
+    
+    #final_clip = concatenate_videoclips(frames)
+
+    
 
     
 
